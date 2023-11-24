68fd19cce5249932278a7a1d42f6c989da4398b3
Vulsan Bianca Maria <biavulsan@yahoo.com>
2023-11-24 22:18:15 +0200

    device simulator works

--- device-simulator/src/main/java/MeasurementsReader.java
+++ device-simulator/src/main/java/MeasurementsReader.java
@@ -1,2 +1,67 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeoutException;
+
 public class MeasurementsReader {
+
+    private static final String CSV_FILE_PATH = "./src/main/resources/sensor.csv";
+
+    public static void main(String[] args) {
+        MeasurementsReader csvReader = new MeasurementsReader();
+        csvReader.readCsvValues();
+    }
+
+    public void readCsvValues() {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new ReadCsvTask(), 0, 1000); // Read one value per second
+
+        // Stop after 10 seconds
+         try {
+             Thread.sleep(10000);
+             timer.cancel();
+         } catch (InterruptedException e) {
+             e.printStackTrace();
+         }
+    }
+
+    private class ReadCsvTask extends TimerTask {
+        private Path csvFilePath = Paths.get(CSV_FILE_PATH);
+
+        @Override
+        public void run() {
+            try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath.toFile()))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    // Assuming the values are separated by commas (CSV)
+                    String[] values = line.split(",");
+                    // Assuming the double value is in the first column
+                    double doubleValue = Double.parseDouble(values[0]);
+
+                    // Do something with the double value, e.g., print it
+                    System.out.println("Read value: " + doubleValue);
+
+                    // Send the double value to the RabbitMQ queue
+                    RabbitMQ_Sender.send(doubleValue);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException(e);
+            } catch (KeyManagementException e) {
+                throw new RuntimeException(e);
+            } catch (TimeoutException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
--- device-simulator/src/main/java/RabbitMQ_Sender.java
+++ device-simulator/src/main/java/RabbitMQ_Sender.java
@@ -10,16 +10,20 @@ import java.util.concurrent.TimeoutException;
 
 public class RabbitMQ_Sender {
 
-    public static void main(String[] args) throws IOException, TimeoutException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
+    public static void send(double value) throws IOException, TimeoutException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setUri("amqps://torcoaxu:bphipUMqDPfi6qPCsFrX6zi8FZe6fVRV@hog.rmq5.cloudamqp.com/torcoaxu");
 
-        try(Connection connection = factory.newConnection()) {
+        try (Connection connection = factory.newConnection()) {
             Channel channel = connection.createChannel();
-//            channel.queueDeclare("measurements-queue", false, false, false, null);
-            String message = "Hello World!";
-            channel.basicPublish("", "measurements-queue", null, message.getBytes());
-            System.out.println(" [x] Sent '" + message + "'");
+            // channel.queueDeclare("measurements-queue", false, false, false, null);
+
+            // Convert double to byte array
+            byte[] valueBytes = Double.toString(value).getBytes();
+
+            // Publish the byte array
+            channel.basicPublish("", "measurements-queue", null, valueBytes);
+            System.out.println(" [x] Sent value: " + value);
         }
     }
 }
