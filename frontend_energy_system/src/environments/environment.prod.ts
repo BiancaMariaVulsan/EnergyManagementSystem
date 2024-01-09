69c3ae94aa35cd70dfb33a3d4201de03db28770a
Stancu Gabriel <gabriel.stancu07@yahoo.com>
2024-01-09 18:52:04 +0200

    Fixed urls for client

--- frontend_energy_system/src/environments/environment.prod.ts
+++ frontend_energy_system/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  personApiUrl: 'http://users-ms-bv.b7gcdzh6bmd0hgee.switzerlandwest.azurecontainer.io:8080',
-  deviceApiUrl: 'http://devices-ms-bv.e3eabjgxgchfaca5.switzerlandwest.azurecontainer.io:8000',
+  personApiUrl: 'http://users-ms-bv.b7gcdzh6bmd0hgee.switzerlandwest.azurecontainer.io:8080/',
+  deviceApiUrl: 'http://devices-ms-bv.e3eabjgxgchfaca5.switzerlandwest.azurecontainer.io:8000/',
   measurementsApiUrl: 'http://monitoring-ms-bv.bsh2aqg8chcxeves.switzerlandwest.azurecontainer.io:8001/'
 };
--- frontend_energy_system/src/environments/environment.ts
+++ frontend_energy_system/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  personApiUrl: 'http://users-ms-bv.b7gcdzh6bmd0hgee.switzerlandwest.azurecontainer.io:8080',
-  deviceApiUrl: 'http://devices-ms-bv.e3eabjgxgchfaca5.switzerlandwest.azurecontainer.io:8000',
+  personApiUrl: 'http://users-ms-bv.b7gcdzh6bmd0hgee.switzerlandwest.azurecontainer.io:8080/',
+  deviceApiUrl: 'http://devices-ms-bv.e3eabjgxgchfaca5.switzerlandwest.azurecontainer.io:8000/',
   measurementsApiUrl: 'http://monitoring-ms-bv.bsh2aqg8chcxeves.switzerlandwest.azurecontainer.io:8001/'
 };
 
